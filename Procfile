d34911d415133e9f782f597f4acb2833dbb573d2
sathya narayanan <contact@sathya.me>
2013-04-27 11:15:48 +0530

    changs procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app.py:create_app\(\) -b 0.0.0.0:$PORT -w 3
+web: python app.py
--- app.py
+++ app.py
@@ -84,4 +84,6 @@ config_dict = {
       }
 
 
+cherrypy.config.update({'server.socket_host': '0.0.0.0',})
+cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
 cherrypy.quickstart(PageLoader(), config = config_dict)
